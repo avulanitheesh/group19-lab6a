64f7ca951e43e82225199f9fd68432bfb0e27002
avulanitheesh <ee24mt032@iitdh.ac.in>
2024-09-22 20:22:04 +0530

    caluculated duty cycle and create functions for handlers

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
 #include <stdint.h>
 #include "tm4c123gh6pm.h"
+volatile uint32_t dutyCycle = 50;  // Initial duty cycle: 50%
+volatile uint32_t count = 160;      // Count value for 100 kHz PWM with 16 MHz clock PWM Frequency=
+
+  // Function declarations
+void GPIO_Handler(void);
+void Systick_Handler(void);
 
